1b2d1d70ba80a23e2580524b9c4b69cc967444b4
wazedkhan <wazedkhan119399@gmail.com>
2025-03-22 23:37:35 +0600

    learn(first-class and higher-order funcs): using func as argument

--- functions/func_as_value.go
+++ functions/func_as_value.go
@@ -2,21 +2,25 @@ package main
 
 import "fmt"
 
+// first-class citizen function
 func add(x, y int) (sum int) {
 	sum = x + y
 	return
 }
 
+// first-class citizen function
 func mul(x, y int) (mul int) {
 	mul = x * y
 	return
 }
 
+// Higher-Order function
 func aggregate(a, b, c int, arithmetic func(int, int) int) (secondResult int) {
 	firstResult := arithmetic(a, b)
 	secondResult = arithmetic(firstResult, c)
 	return
 }
+
 
 func main() {
 	sum := aggregate(2, 3, 4, add)
