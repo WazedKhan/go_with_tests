c59bfedda5ba71a6829542e53d8abef024731a41
wazedkhan <wazedkhan119399@gmail.com>
2025-08-27 22:51:24 +0600

    feat: iteration with benchmark

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,17 +3,28 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat a character 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		// The Repeat function is expected to repeat the character 'a' 5 times
+		expected := "aaaaa"
+		if repeated != expected {
+			t.Errorf("expected '%q' but got '%q'", expected, repeated)
+		}
+	})
 
-	if repeated != expected {
-		t.Errorf("expected '%q' but got '%q'", expected, repeated)
-	}
+	t.Run("repeat a character 0 times", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		// The Repeat function is expected to return an empty string when repeatCount is 0
+		expected := ""
+		if repeated != expected {
+			t.Errorf("expected '%q' but got '%q'", expected, repeated)
+		}
+	})
 
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for b.Loop() {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
